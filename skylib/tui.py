15d09c46ce77ca4c939d296c663c4dfd20fe8930
Jeon Hojin <36477282+SorameHato@users.noreply.github.com>
2023-05-19 15:26:51 +0900

    fixedWidth 추가

--- skylib/tui.py
+++ skylib/tui.py
@@ -55,3 +55,37 @@ def fullWidth(leftText:str,rightText:str):
     write(' '*blanks)
     write(rightText)
     write('\n')
+
+    def fixedWidth(text:str,length:int,array:int=0):
+        '''입력받은 텍스트를 왼쪽 정렬, 가운데 정렬, 또는 오른쪽 정렬로 해서 고정된 넓이로 출력하는 함수
+        예를 들어 길이 11로 가나다라라는 텍스트를 출력하는 경우
+        array : 0 (왼쪽 정렬)   '가나다라   '
+        array : 1 (가운데 정렬) ' 가나다라  ' (만약 글자수가 정확히 반으로 안 나누어 떨어지면 (2의 배수가 아니면) 왼쪽으로, 공백을 오른쪽이 더 많게) : 추후 구현 예정
+        array : 2 (오른쪽 정렬) '   가나다라'
+        array : 3 (가운데 정렬) '  가나다라 ' (만약 글자수가 정확히 반으로 안 나누어 떨어지면 (2의 배수가 아니면) 오른쪽으로, 공백을 왼쪽이 더 많게) : 추후 구현 예정
+        fullWidth를 적용하긴 조금 그렇고 각 열의 데이터의 글자수가 일정한 열 3개 이상의 표를 만들 때 유용
+        만약 text의 길이가 length보다 길면 그냥 그 text 자체를 추가 공백 없이 return 함'''
+        # 먼저 text의 길이를 체크
+        txtLen = __charLen__(text)
+        # 그 다음 text의 길이가 length보다 긴지 아닌지 체크
+        if txtLen >= length:
+            return text
+        # 그 다음 array의 종류를 체크, 0이나 2면 단순히 txtLen - length 만큼의 공백을 붙인 str열을 return
+        # 1이나 3이면 txtLen - length가 짝수면 단순히 양쪽에 int((txtLen - length) / 2) 만큼의 공백을 붙여서 출력하고
+        # 만약 홀수라면 array가 1이면 왼쪽은 int((txtLen - length) / 2) 오른쪽은 int((txtLen - length) / 2) + 1
+        #              array가 3이면 왼쪽은 int((txtLen - length) / 2) + 1 오른쪽은 int((txtLen - length) / 2)
+        if array == 0:
+            return text + (' '*(length - txtLen))
+        elif array == 2:
+            return (' '*(length - txtLen)) + text
+        elif array == 1 or array == 3:
+            halfLen = int((length - txtLen) / 2)
+            if (length - txtLen)%2 == 0:
+                return (' '*halfLen) + text + (' '*halfLen)
+            else:
+                if array == 1:
+                    return (' '*halfLen) + text + (' '*(halfLen+1))
+                else:
+                    return (' '*(halfLen+1)) + text + (' '*halfLen)
+        else:
+            return text
